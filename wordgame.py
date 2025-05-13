29973f6b68122de7d3d5d68a2a615678537ac659
James-bux <jamesbuxton2906@gmail.com>
2025-05-13 12:02:00 +0100

    Update wordgame.py

--- wordgame.py
+++ wordgame.py
@@ -3,3 +3,8 @@ import random
 words = open("wordList.txt", "r")
 lineofword = random.randint(1, 6394)
 
+word = words.readline(lineofword)
+lengthofword = len(word)
+print(word)
+print(lengthofword) #debugging prints
+print(lineofword)
